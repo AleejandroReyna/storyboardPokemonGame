f7badaa5c609651b8b774724d356120e6148397a
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-04 00:52:05 -0600

    dev: vinculated interface with view controller

--- who is that pokemon/PokemonViewController.swift
+++ who is that pokemon/PokemonViewController.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 class PokemonViewController: UIViewController {
-
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet var answerButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
 }
