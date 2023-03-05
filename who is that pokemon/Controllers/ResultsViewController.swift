2077577e5cf9e51f1af33f5bb73e8105b50f106b
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-05 14:02:13 -0600

    dev: Prepared results view
    Added storyboard elements to controller, added properties and behavior of button

--- who is that pokemon/Controllers/ResultsViewController.swift
+++ who is that pokemon/Controllers/ResultsViewController.swift
@@ -6,24 +6,35 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ResultsViewController: UIViewController {
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var pokemonLabel: UILabel!
+    @IBOutlet weak var pokemonImage: UIImageView!
+    
+    var pokemonName : String = ""
+    var pokemonImageURL : String = ""
+    var finalScore : Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scoreLabel.text = "Perdiste, tu puntaje fue de \(self.finalScore)."
+        pokemonLabel.text = "No, es \(pokemonName)"
+        pokemonImage.kf.setImage(with: URL(string: pokemonImageURL))
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func playAgainPress(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
     /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
+    
     */
 
 }
