c22a13a5f9044d3a67cf49d5c6ba514b8c218201
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-04 23:58:24 -0600

    dev: Added pokemon manager and delegate functionalities

--- who is that pokemon/Models/Pokemon/PokemonManager.swift
+++ who is that pokemon/Models/Pokemon/PokemonManager.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+protocol PokemonManagerDelegate {
+    func didUpdatePokemon(pokemons : [PokemonModel])
+    func didFailWithError(error : Error)
+}
+
+struct PokemonManager {
+    let pokemonURL : String = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+    var delegate : PokemonManagerDelegate?
+    
+    func fetchData(){
+        performRequest(with: pokemonURL)
+    }
+    
+    
+    private func performRequest(with uri : String) {
+        if let url = URL(string : uri) {
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { data , response ,err in
+                if(err != nil) {
+                    self.delegate?.didFailWithError(error: err!)
+                }
+                if let safeData = data {
+                    if let pokemon = self.parseJSON(pokemonData: safeData) {
+                        self.delegate?.didUpdatePokemon(pokemons: pokemon)
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    private func parseJSON(pokemonData : Data) -> [PokemonModel]? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(PokemonData.self, from: pokemonData)
+            let pokemon = decodedData.results?.map{
+                PokemonModel(name: $0.name ?? "", imageURL: $0.url ?? "")
+                
+            }
+            return pokemon
+        } catch {
+            return nil
+        }
+    }
+}
